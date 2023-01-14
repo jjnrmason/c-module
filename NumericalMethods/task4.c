f562f334c478967281a917394ec87832cdaae004
John Mason <jjnrmason@me.com>
2023-01-14 08:20:14 +0000

    Start of image blur task

--- NumericalMethods/task4.c
+++ NumericalMethods/task4.c
@@ -13,14 +13,90 @@ aware that the coordinate values will depend on how you format your 2D array):
 The shaded region above represents the pixel we want to blur, in this case, we are focusing on pixel 
 1,2 (x,y) (Centre of the matrix). to apply the blur for this pixel, you would sum all the Red values from 
 the surrounding coordinates including 1,2 (total of 9 R values) and find the average (divide by 9). This 
-is now the new Red value for coordinate 1,2. You must then repeat this for Green and Blue values. 
+is now the new Red value for coordinate 1,2. You must then repeat this for Green and Blue values.
+
 This must be repeated throughout the image. If you are working on a pixel which is not fully 
 surrounded by pixels (8 pixels), you must take the average of however many neighbouring pixels 
 there are. 
+
 NOTE – this program should work with any amount of threads.
+
 Reading in an image file into a single or 2D array (10 marks)
 Applying Box filter on image (20 marks)
 Using multithreading appropriately to apply Box filter (40 marks)
 Using dynamic memory – malloc (10 marks)
 Outputting the correct image with Box Blur applied (20 marks)
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "lodepng.h"
+
+typedef struct {
+    int thread;
+    int start;
+    int end;
+    unsigned int width;
+    unsigned int height;
+    unsigned char* image;
+} tdata;
+
+void *blurImage(void* targs) {
+    pthread_exit(0);
+}
+
+int main(int argc, char** argv) {
+    if (argc != 4) {
+        printf("Program run with the incorrect args.\n");
+        printf("Arg1: Image filename\n");
+        printf("Arg2: New image filename\n");
+        printf("Arg3: Number of threads\n");
+        return 0;
+    }
+
+    char* filename = argv[1];
+    char* newFilename = argv[2];
+    int numberOfThreads = atoi(argv[3]);
+
+    unsigned char* image;
+    unsigned int width, height, error;
+
+    error = lodepng_decode32_file(&image, &width, &height, filename);
+
+    if (error) {
+        printf("Error: %d\n", lodepng_error_text(error));
+        return 0;
+    }
+
+    tdata threadData[numberOfThreads];
+    pthread_t threads[numberOfThreads];
+    int start = 0, end = 0;
+
+    for (int i = 0; i < numberOfThreads; i++) {
+        if (i == 0) {
+            start = 0
+        } else {
+            start += chunkSize;
+        }
+
+        end = start + chunkSize - 1;
+
+        if (i == numberOfThreads - 1) {
+            end = something hmm;
+        }
+
+        threadData[i].start = start;
+        threadData[i].end = end;
+        threadData[i].width = width;
+        threadData[i].height = height;
+        threadData[i].image = image;
+
+        pthread_create(threads[i], NULL, blurImage, &threadData[i]);
+        pthread_join(threads[i], NULL);
+    }
+
+    lodepng_encode32_file(newFilename, image, width, height);
+
+    free(image); // Free pointer
+    return 0;
+}
