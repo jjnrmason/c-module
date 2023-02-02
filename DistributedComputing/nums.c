3efdc0c7c641c21e0482e17c2c585fa645d5a066
John Mason <jjnrmason@me.com>
2023-02-02 14:12:49 +0000

    Update MPI

--- DistributedComputing/nums.c
+++ DistributedComputing/nums.c
@@ -7,34 +7,34 @@ Run with "mpiexec ./nums" or mpiexec -n 4 -oversubscribe ./nums"
 #include <mpi.h>
 
 // Count all the numbers in the slice
-void countNums(float* nums, int chunksize, float* numCount) {
+void countNums(double* nums, int chunksize, double* numCount) {
     for (int i = 0; i < chunksize; i++) {
-        float f = nums[i];
-        *numCount += f;
+        double d = nums[i];
+        *numCount += d;
     }
 }
 
 // Find the average of the slice
-void findAverage(int chunksize, float total, float* average) {
+void findAverage(int chunksize, double total, double* average) {
     *average = total / chunksize;
 }
 
 // Find the biggest number in the slice
-void findMax(float* nums, int chunksize, float* maxNum) {
+void findMax(double* nums, int chunksize, double* maxNum) {
     for (int i = 0; i < chunksize; i++) {
-        float f = nums[i];
-        if (f > *maxNum) {
-            *maxNum = f;
+        double d = nums[i];
+        if (d > *maxNum) {
+            *maxNum = d;
         }
     }
 }
 
 // Find the smallest number in the slice
-void findMin(float* nums, int chunksize, float* minNum) {
+void findMin(double* nums, int chunksize, double* minNum) {
     for (int i = 0; i < chunksize; i++) {
-        float f = nums[i];
-        if (f < *minNum) {
-            *minNum = f;
+        double d = nums[i];
+        if (d < *minNum) {
+            *minNum = d;
         }
     }
 }
@@ -46,19 +46,19 @@ int main(int argc, char** argv) {
     int rank;
     int chunksize;
 
-    float val = 0.0;
-    float* nums;
+    double val = 0.0;
+    double* nums;
     char c;
 
     // Sums for the whole program
-    float numCount;
-    float totalNumCount = 0.0;
-    float numAverage;
-    float finalNumAverage = 0.0;
-    float maxNum;
-    float finalMaxNum = 0.0;
-    float minNum;
-    float finalMinNum = 0.0;
+    double numCount;
+    double totalNumCount = 0.0;
+    double numAverage;
+    double finalNumAverage = 0.0;
+    double maxNum;
+    double finalMaxNum = 0.0;
+    double minNum;
+    double finalMinNum = 0.0;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -89,7 +89,7 @@ int main(int argc, char** argv) {
         printf("File opened\n");
 
         while (!feof(file)){
-            if (fscanf(file, "%f%c", &val, &c) > 0) {
+            if (fscanf(file, "%lf%c", &val, &c) > 0) {
                 count++;
             }
         }
@@ -101,12 +101,12 @@ int main(int argc, char** argv) {
 
         // Use malloc to allocate memory to fit file contents in pointer
         printf("Reading file contents into memory\n");
-        nums = malloc(count * sizeof(float));
+        nums = malloc(count * sizeof(double));
         file = fopen("NumbersForMPI.txt", "r");
 
         // Read floats and add to nums array 
         for (int i = 0; i < count; i++) {
-            fscanf(file, "%f%c", &(nums[i]), &c);
+            fscanf(file, "%lf%c", &(nums[i]), &c);
         }
 
         fclose(file);
@@ -120,7 +120,7 @@ int main(int argc, char** argv) {
         // Send the contents to each rank that's not 0
         for (int i = 1; i < size; i++) {
             MPI_Send(&chunksize, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(&(nums[i * chunksize]), chunksize, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&(nums[i * chunksize]), chunksize, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
         }
 
         // Perform the work for rank 0
@@ -138,46 +138,47 @@ int main(int argc, char** argv) {
 
         // Retrieve the values from the other ranks and add to the totals
         for (int i = 1; i < size; i++) {
-            MPI_Recv(&numCount, 1, MPI_FLOAT, i, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+            MPI_Recv(&numCount, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
             totalNumCount += numCount;
 
-            MPI_Recv(&numAverage, 1, MPI_FLOAT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&numAverage, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             finalNumAverage += numAverage;
 
-            MPI_Recv(&maxNum, 1, MPI_FLOAT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&maxNum, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             if (maxNum > finalMaxNum) {
                 finalMaxNum = maxNum;
             }
 
-            MPI_Recv(&minNum, 1, MPI_FLOAT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&minNum, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             if (minNum < finalMinNum) {
                 finalMinNum = minNum;
             }
         }
 
-        printf("Total count = %f\n", totalNumCount);
+        // Final results may be slightly different due to decimal rounding.
+        printf("Total count = %lf\n", totalNumCount);
 
         finalNumAverage = finalNumAverage / size;
-        printf("Final average = %f\n", finalNumAverage);
+        printf("Final average = %lf\n", finalNumAverage);
 
-        printf("Final max num = %f\n", finalMaxNum);
+        printf("Final max num = %lf\n", finalMaxNum);
 
-        printf("Final min num = %f\n", finalMinNum);
+        printf("Final min num = %lf\n", finalMinNum);
     } else {
         // Receive the values from rank 0 and perform the work
         MPI_Recv(&chunksize, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        nums = malloc(chunksize * sizeof(float));
-        MPI_Recv(nums, chunksize, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        nums = malloc(chunksize * sizeof(double));
+        MPI_Recv(nums, chunksize, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         countNums(nums, chunksize, &numCount);
         findAverage(chunksize, numCount, &numAverage);
         findMax(nums, chunksize, &maxNum);
         findMin(nums, chunksize, &minNum);
 
-        MPI_Send(&numCount, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
-        MPI_Send(&numAverage, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
-        MPI_Send(&maxNum, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
-        MPI_Send(&minNum, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&numCount, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&numAverage, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&maxNum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&minNum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
